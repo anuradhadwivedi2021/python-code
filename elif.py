138a89d349a5ae597846682381ba443664600939
anuradha dilip dwivedi <anuradha.dwivedi2021@gmail.com>
2025-04-05 23:39:01 +0530

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,9 @@
+marks=int(input("enter your marks:"))
+if marks >= 90:
+    print("grade:34 A+")
+elif marks >=80:
+    print("grade: b")
+elif marks >=70:
+    print("grade: c")
+else:
+    print("grade: d")            
